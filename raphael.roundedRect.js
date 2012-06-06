b1f59b34bed0272b66d6360b888b9fbb4c21c7cb
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-06-06 17:10:16 +0200

    Shorten the code
    Inspired by http://jsdo.it/remmel/1qGu

--- raphael.roundedRect.js
+++ raphael.roundedRect.js
@@ -1,5 +1,7 @@
 /**
  * Draws a rounded rectangle, where you can specify the radius of each corner
+ *
+ * If you supply only r1, all corners will have the same radius as r1
  * 
  * @param {Number} x
  * 		The top left x coordinate
@@ -9,40 +11,25 @@
  * 		The width of the rectangle 
  * @param {Number} height 
  * 		The height of the rectangle
- * @param {Number} radiusTopLeft
- * 		The corner radius of the top left corner. Defaults to 0;
- * @param {Number} radiusTopRight
- * 		The corner radius of the top right corner. Defaults to 0;
- * @param {Number} radiusBottomRight
- * 		The corner radius of the bottom right corner. Defaults to 0;
- * @param {Number} radiusBottomLeft
- * 		The corner radius of the bottom left corner. Defaults to 0;
+ * @param {Number} r1
+ * 		The corner radius of the top left corner.
+ * @param {Number} r2
+ * 		The corner radius of the top right corner.
+ * @param {Number} r3
+ * 		The corner radius of the bottom right corner.
+ * @param {Number} r4
+ * 		The corner radius of the bottom left corner.
  */
-Raphael.fn.roundedRect = function(x, y, width, height, radiusTopLeft, radiusTopRight, radiusBottomRight, radiusBottomLeft) {
-	if (typeof radiusBottomLeft === "undefined") {
-	    radiusBottomLeft = 0;
-	}
-	if (typeof radiusBottomRight === "undefined") {
-	    radiusBottomRight = 0;
-	}
-	if (typeof radiusTopRight === "undefined") {
-	    radiusTopRight = 0;
-	}
-	if (typeof radiusTopLeft === "undefined") {
-	    radiusTopLeft = 0;
-	}
-	
-	var path = 
-	"M" + (x + radiusTopLeft) + "," + y + 
-	" l" + (width - radiusTopLeft - radiusTopRight) + "," + 0 +
-	" q" + radiusTopRight + "," + 0 + "," + radiusTopRight + "," + radiusTopRight +
-	" l" + 0 +","+ (height - radiusTopRight - radiusBottomRight) +
-	" q" + 0 + "," + radiusBottomRight + "," + (-radiusBottomRight) + "," + (radiusBottomRight) + 
-	" l" + (-(width - radiusBottomRight - radiusBottomLeft)) + "," + 0 +
-	" q" + (-radiusBottomLeft) + "," + 0 + "," + (-radiusBottomLeft) + "," + (-radiusBottomLeft) + 
-	" l" + 0 + "," + (-(height - radiusBottomLeft - radiusTopLeft)) + 
-	" q" + 0 + "," + (-radiusTopLeft) + "," + radiusTopLeft + "," + (-radiusTopLeft) + 
-	" Z"; 
-	
-	return this.path(path);
+Raphael.fn.roundedRect = function (x, y, w, h, r1, r2, r3, r4){
+    if (r1 !== undefined && !(r2 || r3 || r4 )) {
+        r2 = r3 = r4 = r1;
+    }
+
+    var array = [];
+    array = array.concat(["M",x,r1+y, "Q",x,y, x+r1,y]); //A
+    array = array.concat(["L",x+w-r2,y, "Q",x+w,y, x+w,y+r2]); //B
+    array = array.concat(["L",x+w,y+h-r3, "Q",x+w,y+h, x+w-r3,y+h]); //C
+    array = array.concat(["L",x+r4,y+h, "Q",x,y+h, x,y+h-r4, "Z"]); //D
+
+    return this.path(array);
 };
